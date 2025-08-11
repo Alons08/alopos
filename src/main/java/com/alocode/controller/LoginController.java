609a75d9221bf27d4dad6e6b848528f65ebc657a
Your Name <you@example.com>
2025-08-11 15:46:16 -0500

    Eliminar la inyección de dependencias manual en LoginController

--- src/main/java/com/alocode/controller/LoginController.java
+++ src/main/java/com/alocode/controller/LoginController.java
@@ -5,7 +5,6 @@ import com.alocode.repository.UsuarioRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
